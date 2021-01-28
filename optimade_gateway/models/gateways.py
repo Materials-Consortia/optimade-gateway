883b91cca48eee5ad396482882f1c355d0596a8a
Casper Welzel Andersen <casper.andersen@epfl.ch>
2021-01-28 12:23:30 +0100

    Don't allow dbs with special link types
    Link types "root" and "providers" _only_ refer to index meta-dbs,
    something we're not interested in from a gateway resource.
    The last two link types can refer to both types of OPTIMADE dbs (index
    and regular).

--- optimade_gateway/models/gateways.py
+++ optimade_gateway/models/gateways.py
@@ -1,7 +1,8 @@
 from typing import List
 
 from optimade.models import EntryResource, EntryResourceAttributes, LinksResource
-from pydantic import Field
+from optimade.models.links import LinkType
+from pydantic import Field, validator
 
 
 class GatewayResourceAttributes(EntryResourceAttributes):
@@ -11,6 +12,21 @@ class GatewayResourceAttributes(EntryResourceAttributes):
         ...,
         description="List of databases (OPTIMADE 'links') to be queried in this gateway.",
     )
+
+    @validator("databases")
+    def no_index_databases(cls, v):
+        """Ensure databases are not of type "root" and "providers"
+
+        NOTE: Both "external" and "child" can still represent index meta-dbs,
+        but "root" and "providers" can not represent "regular" dbs.
+        """
+        for db in v:
+            if db.attributes.link_type in (LinkType.ROOT, LinkType.PROVIDERS):
+                raise ValueError(
+                    "Databases with 'root' or 'providers' link_type is not allowed for gateway resources. "
+                    f"Given database: {db}"
+                )
+        return v
 
 
 class GatewayResource(EntryResource):
