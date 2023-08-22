0039c5ed80da6ecc868def5eb97cb99d9cfb4dc7
Casper Welzel Andersen <casper.w.andersen@sintef.no>
2023-08-22 13:50:06 +0200

    Remove version constraints for pytest add-ons
    This leads to the removal of the related invoke task as well as the
    removal of several steps in the auto-merge dependabot PR CI workflow.

--- tasks.py
+++ tasks.py
@@ -13,8 +13,6 @@ from invoke import task
 if TYPE_CHECKING:
     from typing import Optional, Tuple
 
-    from invoke import Context, Result
-
 
 TOP_DIR = Path(__file__).parent.resolve()
 
@@ -66,78 +64,3 @@ def setver(_, ver=""):
     )
 
     print(f"Bumped version to {ver}")
-
-
-@task
-def update_pytest_reqs(_):
-    """Update the pytest plugins to be minimum the currently listed requirement
-    versions."""
-    from copy import deepcopy
-
-    config = TOP_DIR / "pyproject.toml"
-    requirements = TOP_DIR / "requirements_dev.txt"
-
-    # Retrieve dependencies specified in the config file
-    with open(config, encoding="utf8") as handle:
-        for line in handle.readlines():
-            plugins = re.match(r'^required_plugins = "(?P<plugins>.*)".*', line)
-            if plugins:
-                break
-        else:
-            raise RuntimeError(
-                "Couldn't find the required plugins for pytest in the config file at "
-                f"{config} !"
-            )
-
-    plugins = {
-        dependency.group("name"): dependency.group("version")
-        for dependency in [
-            re.match(r"^(?P<name>[a-z-]+)>=(?P<version>[0-9]+(\.[0-9]+){1,2})$", _)
-            for _ in plugins.group("plugins").split(" ")  # type: ignore[union-attr]
-        ]
-        if dependency
-    }
-    original_versions = deepcopy(plugins)
-
-    # Update the retrieved versions with those from the requirements file
-    dependencies_found_counter = 0
-    with open(requirements, encoding="utf8") as handle:
-        for line in handle.readlines():
-            for plugin in plugins:
-                dependency = re.match(
-                    rf"^{plugin}~=(?P<version>[0-9]+(\.[0-9]+){{1,2}}).*", line
-                )
-                if not dependency:
-                    continue
-                dependencies_found_counter += 1
-                plugins[plugin] = dependency.group("version")
-
-    # Sanity check
-    if dependencies_found_counter != len(plugins):
-        raise RuntimeError(
-            f"Did not find all specified dependencies from the config file ({config}) in "
-            f"the development requirements file ({requirements}).\nDependencies found in "
-            f"the requirements file: {dependencies_found_counter}\nDependencies found in "
-            f"the config file: {len(plugins)}"
-        )
-
-    # Update the config file dependency versions (if necessary)
-    for plugin in original_versions:
-        if original_versions[plugin] != plugins[plugin]:
-            break
-    else:
-        print("No updates detected; the config file is up-to-date.")
-        sys.exit()
-
-    update_file(
-        config,
-        (
-            r"^required_plugins = .*",
-            'required_plugins = "'
-            f'{" ".join(f"{name}>={version}" for name, version in plugins.items())}"',
-        ),
-    )
-    print(
-        f"Successfully updated pytest config plugins:\n        {plugins}\n  (was: "
-        f"{original_versions})"
-    )
